bec739c8caf2cdf1c12d0b4fb7291fb5e2088278
Zakharov <ihor.o.zakharov@outlook.com>
2025-05-26 00:59:47 +0300

    Add project files.

---
+++ lab10/Athlete.h
@@ -0,0 +1,96 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+
+template <typename T>
+class Athlete
+{
+	public:
+		std::string name;
+		T field;
+		static int S;
+
+		Athlete(T field, const std::string& name) : field(field), name(name) {};;
+
+		virtual void Exercise()
+		{
+			std::cout << "Exercise athlete" << std::endl;
+		}
+
+		virtual ~Athlete() 
+		{
+			std::cout << "Athlete class has been deleted";
+		};
+};
+
+template <typename T>
+class Swimmer : virtual public Athlete<T>
+{
+	public:
+		using Athlete<T>::Athlete; // copying constructors from Athlete class
+
+		void Exercise() override
+		{
+			std::cout << "Exercise swimmer" << std::endl;
+		}
+
+		template<typename... Args>
+		void test(const Args&... args)
+		{
+			(std::cout << ... << args);
+		}
+};
+
+template<int N>
+struct Fibonacci
+{
+	static constexpr int value = Fibonacci<N - 1>::value + Fibonacci<N - 2>::value; // constexpr - computed in compile time when possible
+};
+
+template<>
+struct Fibonacci<1>
+{
+	static constexpr int value = 1;
+};
+
+template<>
+struct Fibonacci<0>
+{
+	static constexpr int value = 1;
+};
+
+template <typename T>
+class Runner : virtual public Athlete<T>
+{
+public:
+	using Athlete<T>::Athlete; // copying constructors from Athlete class
+
+	void Exercise() override
+	{
+		std::cout << "Exercise runner" << std::endl;
+	}
+
+	template <int N>
+	void test()
+	{
+		std::cout << Fibonacci<N>::value << std::endl;
+	}
+};
+
+template <typename T>
+class Champion final : public Swimmer<T>, public Runner<T>
+{
+public:
+	Champion(T field, const std::string& name)
+		: Athlete<T>(field, name),
+		Swimmer<T>(field, name),
+		Runner<T>(field, name) {};
+
+	void Exercise() override
+	{
+		std::cout << "Exercise champion" << std::endl;
+	}
+};
+
+template <typename T>
+int Athlete<T>::S = 5;
---
+++ lab10/lab10.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include "Athlete.h"
+
+struct A
+{
+
+};
+
+struct B
+{
+    void b() {};
+};
+
+struct C
+{
+    virtual void b() {};
+};
+
+int main() 
+{
+    Athlete<int> athlete1 = Swimmer<int>(12, "abc");
+
+    std::cout << "Athlete 1 exercise: ";
+    athlete1.Exercise();
+
+    Swimmer<int> athlete2 = Swimmer<int>(12, "abc");
+
+    std::cout << "Athlete 2 exercise: ";
+    athlete2.Exercise();
+
+    std::cout << "Athlete 2 test (true, false, true): ";
+    athlete2.test(true, false, true);
+    std::cout << std::endl;
+
+    std::cout << "Athlete 2 test (12, 34, 56, 78): ";
+    athlete2.test(12, 34, 56, 78);
+    std::cout << std::endl;
+
+    Runner<int> runner = Runner<int>(12, "abc");
+    runner.test<5>();
+
+    std::cout << "sizeof A: " << sizeof(A) << std::endl;
+    std::cout << "sizeof B: " << sizeof(B) << std::endl; 
+    std::cout << "sizeof C: " << sizeof(C) << std::endl; // class info link
+    
+    Champion<std::string>* champion = new Champion<std::string>("abc", "cba");
+    champion->Exercise();
+
+    return 0;
+}
